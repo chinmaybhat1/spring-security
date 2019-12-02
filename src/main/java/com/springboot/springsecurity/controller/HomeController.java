95b8a8f020f08c73150a0de3209bb262deaf276a
Chinmay Bhat <chinmay.bhat@cerner.com>
2019-12-02 10:21:03 +0530

    Configure Spring Security Authentication

--- src/main/java/com/springboot/springsecurity/controller/HomeController.java
+++ src/main/java/com/springboot/springsecurity/controller/HomeController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@GetMapping("/")
-	public String helloWorld() {
+	public String home() {
 		return ("<h1>Hello World</h1>");
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return ("<h1>Hello User</h1>");
+	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return ("<h1>Hello Admin</h1>");
 	}
 }
